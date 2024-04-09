e76513dd15dfee9c6bc2b4cfbce71c88bbd9b745
michael-xu-ecen <159245285+michael-xu-ecen@users.noreply.github.com>
2024-04-09 18:14:34 -0500

    Fixed project structure and updated README.

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
 ##  Attacks
 
-### Differential Privacy Based Federated Learning
+
+## repos
+Contains git submodules for each model implementation
+Visit each repositories README for installation and running instructions.
+iDash-2020, DPBFL, and GradAttack were all successfully setup and run.
+
+## data
+Contains BC-TCGA dataset as well as 3-D image datasets for testing.
+Currently testing 3-D Brain Tumor Images for classification.
 
 
+## src
+### Not fully implemented, code has errors.
+### BCTCGADataModule.py
+Implementation of LightningDataModule for BC-TCGA. Will be _used regardless of model.
+Implementation of a 3-D dataset DataModule ..._
 
-### GradAttack
+### GradAttack.py
+Example Gradient Inversion attack on CIFAR10 dataset. Taken from GradAttack/examples. Trying to convert this
+to work with BCTCGADataModule.
+
+### DPFLAttack.py
+Example running on MNIST dataset.Trying to integrate GradAttack into this pipeline. Need to wrap model in Lightning.
+Then conver this to work with BCTCGADataModule.
 
 
+## requirements.txt
+### Will include more installation instructions later.
+List of project dependencies. Use pip install -r requirements.txt.
+Preferably install dependencies in virtual environment, either venv or conda.
+Personally used conda for initial setup. Requires manual installation of torchcsprng.
 
-### main.py
+
 
 
---
+++ repos/Differential-Privacy-Based-Federated-Learning/draw.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def openfile(filepath):
+    file = open(filepath)
+    y = []
+    while 1:
+        line = file.readline()
+        if line.rstrip('\n') == '':
+            break
+        y.append(float(line.rstrip('\n')))
+        if not line:
+            break
+        pass
+    file.close()
+    return y
+
+if __name__ == '__main__':
+    mpl.use('TkAgg')
+    plt.figure()
+    epsilon_array = ['1.0', '5.0', '10.0', '20.0', '30.0']
+    plt.ylabel('Testing Accuracy')
+    plt.xlabel('Global Round')
+    for epsilon in epsilon_array:
+        y = openfile('./log/accfile_fed_mnist_cnn_100_iidFalse_dp_Gaussian_epsilon_{}.dat'.format(epsilon))
+        plt.plot(range(100), y, label=r'$\epsilon={}$'.format(epsilon))
+    y = openfile('./log/accfile_fed_mnist_cnn_100_iidFalse_dp_no_dp_epsilon_20.dat'.format(epsilon))
+    plt.plot(range(100), y, label=r'$\epsilon=+\infty$')
+    plt.title('Mnist Gaussian')
+    plt.legend()
+    plt.savefig('mnist_gaussian.png')
+
+    plt.figure()
+    epsilon_array = ['1.0', '5.0', '10.0', '20.0', '30.0']
+    plt.ylabel('Testing Accuracy')
+    plt.xlabel('Global Round')
+    for epsilon in epsilon_array:
+        y = openfile('./log/accfile_fed_mnist_cnn_100_iidFalse_dp_MA_epsilon_{}.dat'.format(epsilon))
+        plt.plot(range(100), y, label=r'$\epsilon={}$'.format(epsilon))
+    y = openfile('./log/accfile_fed_mnist_cnn_100_iidFalse_dp_no_dp_epsilon_20.dat'.format(epsilon))
+    plt.plot(range(100), y, label=r'$\epsilon=+\infty$')
+    plt.title('Mnist Gaussian Moment Account (q = 0.01)')
+    plt.legend()
+    plt.savefig('mnist_gaussian_MA.png')
+
+    plt.figure()
+    epsilon_array = ['10.0', '25.0', '50.0', '75.0', '100.0']
+    plt.ylabel('Testing Accuracy')
+    plt.xlabel('Global Round')
+    for epsilon in epsilon_array:
+        y = openfile('./log/accfile_fed_mnist_cnn_100_iidFalse_dp_Laplace_epsilon_{}.dat'.format(epsilon))
+        plt.plot(range(100), y, label=r'$\epsilon={}$'.format(epsilon))
+    y = openfile('./log/accfile_fed_mnist_cnn_100_iidFalse_dp_no_dp_epsilon_20.dat'.format(epsilon))
+    plt.plot(range(100), y, label=r'$\epsilon=+\infty$')
+    plt.title('Mnist Laplace')
+    plt.legend()
+    plt.savefig('mnist_gaussian_laplace.png')
+
